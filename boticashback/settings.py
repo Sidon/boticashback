1fb009c7b3250547b600df8a937f6976f0030f9a
Sidon <sidoncd@gmail.com>
2020-02-17 16:05:23 -0300

    Configurando heroku

--- boticashback/settings.py
+++ boticashback/settings.py
@@ -122,9 +122,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+
+print('==>', STATIC_ROOT )
+print('--->', STATIC_URL)
+
 
 # --------------------------
 
