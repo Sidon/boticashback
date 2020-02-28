a2ca53b60f92f0328f09aff55c1e449e77c227d8
Sidon <sidoncd@gmail.com>
2020-02-28 20:43:33 -0300

    Problemas com setocale no heroku

--- apps/reseller/models.py
+++ apps/reseller/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from localflavor.br.models import BRCPFField
 
 import locale
-locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+# locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 User = get_user_model()
 
