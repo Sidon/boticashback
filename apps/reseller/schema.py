d4d2d7962e04f77125521d74b68257cc3c20924b
Sidon <sidoncd@gmail.com>
2020-06-27 21:37:38 -0300

    adapting for tech-talck

--- apps/reseller/schema.py
+++ apps/reseller/schema.py
@@ -4,6 +4,7 @@ from graphene_django.types import DjangoObjectType
 from graphene_django.rest_framework.mutation import SerializerMutation
 from django.contrib.auth import get_user_model
 from apps.reseller.models import Reseller
+from django.db.models import Q
 from apps.reseller.serializers import ResellerSerializer
 User = get_user_model()
 
@@ -17,15 +18,40 @@ class ResellerQuery(graphene.ObjectType):
     reseller = graphene.Field(
         ResellerType,
         id=graphene.Int(),
-        cpf=graphene.String(required=True),
-        full_name=graphene.String(required=True),
+        cpf=graphene.String(required=False),
+        full_name=graphene.String(required=False),
+        purchases=graphene.List(graphene.String)
     )
 
     # all_resellers = graphene.List(ResellerType, token=graphene.String(required=True))
-    all_resellers = graphene.List(ResellerType, token=graphene.String(required=False))
+    all_resellers = graphene.List(
+        ResellerType,
+        search=graphene.String(),
+        first=graphene.Int(),
+        skip=graphene.Int(),
+        last=graphene.Int(),
+        token=graphene.String(required=False)
+    )
 
-    def resolve_all_resellers(self, info, **kwargs):
-        return Reseller.objects.all()
+    def resolve_all_resellers(self, info, search=None, first=None, skip=None, last=None, **kwargs):
+        qs = Reseller.objects.all()
+
+        if search:
+            _filter = (
+                Q(full_name__icontains=search)
+            )
+            qs = qs.filter(_filter)
+
+        if last:
+            qs = qs.order_by('-id')[:last]
+        else:
+            if skip:
+                qs = qs[skip:]
+
+            if first:
+                qs = qs[:first]
+
+        return qs
 
     def resolve_reseller(self, info, **kwargs):
         id = kwargs.get('id')
